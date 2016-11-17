7e80f917c4b51708ef6c844ebc2092c0e23b0012
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2016-11-17 16:30:35 -0500

    structure

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 var express = require('express');
 var request = require('request');
 var app = express();
-app.get('/test', function(req,res){
-	res.send("hello world")
+
+app.use('/static',express.static(__dirname + '/public'));
+
+app.get('/', function(req,res){
+	res.sendFile(__dirname + "/views/index.html");
 });
+
 app.listen(8080,function(){
 	console.log('listening on port 8080');
 });
